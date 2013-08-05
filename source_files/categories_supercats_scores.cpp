00ae272d5970350e3bd177f575084ac52b19f101
Daniel Richman <ddr@danielrichman.com>
2013-08-05 16:13:52 -0700

    Compiles, but untested, implementation of random walk with restart using matrix multiplication and armadillo sparse matrices.

--- source_files/categories_supercats_scores.cpp
+++ source_files/categories_supercats_scores.cpp
@@ -5,12 +5,32 @@
 
 	Algorithm: random walk with restart. The heavy time and memory requirements of this algorithm mean that multiple threads are used. 
 
+
+
+	IMPLEMENTATION DETAILS:
+		1) Parse data in addline(). At this stage, we build a network of nodes, each with pointers to parent and child nodes. We also build a hash table (unordered_map)
+			that allows us to quickly find a node object, given the name of the category it represents. 
+		2) Build graph matrix in populate_graph_matrix. At this stage we convert the node matrix into an alternative representation as a sparse matrix. 
+			// Graph structure: G(i,j) represents the probability of travelling from i to j in any given step. 
+			// Since this graph is unweighted, G(i,j) is simply:
+			//			(1 - alpha) / len(neighbors(i)) if i and j are neighbors
+			//			0 					  if i and j are not neighbors
+
+			alpha is the probability (random walk with **restart**) that the random walker returns to the starting node. This is typically ~0.01-0.03. 
+
+			The graph matrix we construct at this stage does not care which node is the starting node. The random walk with restart algo simply has to
+			add alpha to the probability of being at its starting node at each stage of its evolution. 
+
+		3) Execute RWR algorithm for each category in Wikipedia. This is executed in a threaded way for speed. 
+
+
 	Data structure: an unordered_map maps category names to a node *. Each node contains the category name and
 	the name of the node's parents. 
 
 	Written June-August 2013 by Daniel Richman. 
 
-	Note: This program requires C++11 language and library support. Compile with -std=c++11. On Macs (clang++) also use -stdlib=libc++. 
+	Note: This program requires C++11 language and library support. Compile with -std=c++11. On Macs with clang++ also use -stdlib=libc++.
+			Tested with g++ 4.7.2 on linux x86_64 and clang++ 3.3 on OS X. 
 ***************/
 
 #include <algorithm>
@@ -30,7 +50,11 @@
 #include <unordered_set>
 #include <vector>
 
+#define ARMA_64BIT_WORD
+#include <armadillo>
+
 using namespace std;
+using namespace arma;
 
 
 // Represents a single category
@@ -38,8 +62,12 @@ struct node {
 	string name;
 	vector<node *> parents;
 	vector<node *> children;
+
+	unsigned long category_id; // each category has a number assigned by populate_graph_matrix to enable lookups in the sp_fmat category_matrix. 
 };
 
+
+const unsigned long ID_UNASSIGNED = 0;
 
 // Used in the BFS
 const string root_category = "Main_topic_classifications"; // category from which we perform a BFS to visit *all* articles
@@ -52,6 +80,10 @@ const string top_categories[] {"Mathematics", "Language", "Chronology", "Belief"
 "Sports", "Business", "Arts", "Life", "Politics"};
 
 
+// alpha for the random walk with restart
+const float RWR_ALPHA = 0.01;
+
+
 // Synchronizers! 
 mutex cout_lock; // locks writing to cout
 mutex task_reassign_lock; // locks managing the queue of random walks to be performed
@@ -61,7 +93,7 @@ const unsigned MAX_THREADS = 8;
 
 
 // Count # of neighbors of a given node
-int count_neighbors_of(node *n) {
+inline int count_neighbors_of(node *n) {
 	return n->parents.size() + n->children.size();
 }
 
@@ -90,6 +122,7 @@ node *get_or_add_node(const string &name, unordered_map<string, node *> &lookup_
 		// subcat not in the lookup table: make a new node
 		n = new node;
 		n->name = name;
+		n->category_id = ID_UNASSIGNED;
 
 		lookup_table[name] = n;
 	}
@@ -112,18 +145,63 @@ void addline(const string &line, unordered_map<string, node *> &lookup_table) {
 	subcat->parents.push_back(supercat);
 }
 
+// Build graph matrix: M(i,j) = probability of moving along the edge from i to j in a single step. 
+// alpha = probability the random walk with restart returns to the start at any step. 
+void populate_graph_matrix(unordered_map<string, node*> &lookup_table, sp_fmat *category_matrix, float alpha) {
+
+	unsigned long current_category_id = 1;
+
+	for (auto it = lookup_table.cbegin(); it != lookup_table.cend(); it++) {
+
+		unsigned long iteration_start_category_id = current_category_id;
+
+		node *n = it->second;
+		int n_neighbors = count_neighbors_of(n);
+		
+		if (n->category_id == ID_UNASSIGNED) {
+			n->category_id = current_category_id; // assign the category id
+			current_category_id++;
+		}
+
+		// add info about neighbors to graph
+		for (node *p : n->parents) {
+			if (p->category_id == ID_UNASSIGNED) {
+				p->category_id = current_category_id; // assign the category id
+				current_category_id++;
+			}
+
+			(*category_matrix)(n->category_id, p->category_id) = (1.f - alpha) / n_neighbors; // probability of moving from n to p in a single step is 1/count_neighbors_of(n)
+		}
+
+		for (node *c : n->children) {
+			if (c->category_id == ID_UNASSIGNED) {
+				c->category_id = current_category_id; // assign the category id
+				current_category_id++;
+			}
+
+			(*category_matrix)(n->category_id, c->category_id) = (1.f - alpha) / n_neighbors; // probability of moving from n to c in a single step is 1/count_neighbors_of(n)
+		}
+
+
+		if ((iteration_start_category_id % 50000) > (current_category_id % 50000)) // passed a multiple of 50,000 in this iteration
+			cerr << "\tFinished category " << current_category_id << endl;
+
+	}
+}
+
 // Prototype
-void print_random_walk_output(const string &category, const unordered_map<string, float> &scores);
-void reassign_annotation_tasks(unordered_map<string, node *> &lookup_table);
+void print_random_walk_output(const string &category, const unordered_map<string, node *> &lookup_table, sp_fmat &rwr_results);
+void reassign_annotation_tasks(unordered_map<string, node *> &lookup_table, sp_fmat *category_matrix, float alpha);
 
 // Perform Random Walk with Restart, beginning at the specified node in the tree. 
 // Note: start_node_name should not be declared as a reference because of the
 // loop in which it is created. 
 void random_walk(const string start_node_name, // node at which to begin
-				unordered_map<string, node *> &lookup_table)
+				unordered_map<string, node *> &lookup_table,
+				sp_fmat *category_matrix,
+				float alpha)
 {
 
-	float alpha = 0.01; // probability of returning to the top node at any given step)
 	float walk_iterations = 10; // number of random walk iterations performed
 
 	node *start_node;
@@ -138,156 +216,37 @@ void random_walk(const string start_node_name, // node at which to begin
 		return;
 	}
 
-	// Allocate the unordered_map for this node
-	unordered_map<string, float> *rw_probabilities_last, *rw_probabilities_current;
 
-	rw_probabilities_last = new unordered_map<string, float>;
-	rw_probabilities_current = new unordered_map<string, float>;
+	sp_fmat rwr_results(1, lookup_table.size()); // this row matrix
+	rwr_results(1, start_node->category_id) = 1; // there's 100% probability we're at the starting node at t = 0
 
-	(*rw_probabilities_last)[start_node_name] = 1; // At t = 0, there's 100% probability we're at the starting node. If a node is not in the unordered_map at time t, there is 0% probability the random walker is there at time t. 
-
-
-	// Because there are so many nodes, we use a BFS to keep track of which nodes we must compute random-walker probability for. 
-
-	list<node *> *tovisit_now = new list<node *>;
-	list<node *> *tovisit_next = new list<node *>;
-	list<node *> *allvisited = new list<node *>; // keeps track of all visited nodes so we know which random-walker nodes we might have reached by this step
-	
-	unordered_set<node *> visited_set;
-
-	tovisit_now->push_back(start_node);
-	allvisited->push_back(start_node);
-
-	// Perform, walk_iterations times,
-	// a breadth-first-search step followed by a random walker recomputation step
-	for (int j = 0; j < walk_iterations; j++) {
-
-		// Perform one layer of BFS.  
-		while (!tovisit_now->empty()) {
-			node *nextnode = tovisit_now->front();
-			tovisit_now->pop_front();
-
-			// Visit all children and parents of this node. 
-			for (node *child : nextnode->children) {
-
-				// Make sure the node we're now considering has never been visited before and is not already scheduled to be visited. 
-				if (visited_set.find(child) == visited_set.end()) {
-
-					tovisit_next->push_back(child);
-					visited_set.insert(child);
-				}
-			}
-
-			for (node *parent : nextnode->parents) {
-
-				// Make sure the node we're now considering has never been visited before and is not already scheduled to be visited. 
-				if (visited_set.find(parent) == visited_set.end()) {
-
-					tovisit_next->push_back(parent);
-					visited_set.insert(parent);
-				}
-			}
-
-			allvisited->push_back(nextnode);
-		}
-
-		// tovisit_now is now empty. Swap it and tovisit_next: on the next BFS cycle, we will visit all the nodes in what is currently tovisit_next. 
-		list<node *> *tmp = tovisit_now;
-		tovisit_now = tovisit_next;
-		tovisit_next = tmp;
-		
-		// Perform random walk calculations on all nodes in allvisited, since these are the nodes the random walker might have visited by this timestamp
-		for (node *n : *allvisited) {
-
-			float probability = 0; // probability the random walker is at this node at the current timestep
-
-			// For all parent nodes and children nodes of n, add up probabilities that we're now there. 
-			for (node *p : n->parents) {
-				
-				// Look up name in rw_probabilities_last to see whether we recorded a nonzero value. 
-				auto it = rw_probabilities_last->find(p->name);
-
-				// Not in hash table: we couldn't have been at p last iteration. 
-				if (it == rw_probabilities_last->end())
-					continue;
-
-				float parent_last_prob = it->second;
-					
-				probability += parent_last_prob / count_neighbors_of(p);
-			}
-			
-			for (node *c : n->children) {
-				// Look up name in rw_probabilities_last to see whether we recorded a nonzero value. 
-				auto it = rw_probabilities_last->find(c->name);
-
-				// Not in hash table: we couldn't have been at c last iteration. 
-				if (it == rw_probabilities_last->end())
-					continue;
-
-				float child_last_prob = it->second;
-					
-				probability += child_last_prob / count_neighbors_of(c);
-			}
-			
-			// Make final adjustments to probability for n, given that we return randomly to the starting node. 
-			probability *= (1 - alpha);
-			
-			if (n == start_node) {
-				probability += alpha;
-			}
-			
-			(*rw_probabilities_current)[n->name] = probability;
-		}
-		
-		// Next cycle: rw_probabilities_current will become rw_probabilities_last and the old rw_probabilities_last will go away. 
-		unordered_map<string, float> * tmp2 = rw_probabilities_last;
-		tmp2->clear(); // now empty and suitable for reuse
-		
-		rw_probabilities_last = rw_probabilities_current; // save current state
-		rw_probabilities_current = tmp2; // empty
+	// Perform random walks with restart
+	for (short j = 0; j < walk_iterations; j++) {
+		rwr_results *= *category_matrix;
+		rwr_results(1, start_node->category_id) += alpha;
 	}
-	
-	
-	// Reached the end of the random walk iterations. Save the data we're interested in. 
-	unordered_map<string, float> random_walk_connections;
-	
-	// Iterate through all the categories we're interested in and print out their 
-	for (string cat : top_categories) {
-		try {
-			random_walk_connections[cat] = rw_probabilities_last->at(cat);
-		} catch (out_of_range &) {
-			// specified supercat not reached in the random walk
-			random_walk_connections[cat] = 0;
-		}
-	}
+
 
 	// Print data
-	print_random_walk_output(start_node_name, random_walk_connections);
+	print_random_walk_output(start_node_name, lookup_table, rwr_results);
+
 
 	// Inform dispatcher another thread can be created
-	reassign_annotation_tasks(lookup_table);
-
-	// Clean up. 
-	delete tovisit_now;
-	delete tovisit_next;
-	delete allvisited;
-
-	delete rw_probabilities_last;
-	delete rw_probabilities_current;
+	reassign_annotation_tasks(lookup_table, category_matrix, alpha);
 }
 
 
 // Print out the supercat scores vector for this category
-void print_random_walk_output(const string &category, const unordered_map<string, float> &scores) {
+void print_random_walk_output(const string &category, const unordered_map<string, node *> &lookup_table, sp_fmat &rwr_results) {
 
 	lock_guard<mutex> lock(cout_lock);
 
 	cout << category << "> ";
 
 	// Print out each score
-	for (auto it = scores.cbegin(); it != scores.cend(); it++) {
-
-		cout << it->first << ": " << it->second << ", ";
+	for (string topcat : top_categories) {
+		// How does this work? We print out the topcat name. To get the score, we look up in the rwr_results row vector the entry corresponding to the topcat. 
+		cout << topcat << ": " << rwr_results(1, (lookup_table.at(topcat))->category_id) << ", ";
 	}
 
 	cout << endl;
@@ -296,7 +255,7 @@ void print_random_walk_output(const string &category, const unordered_map<string
 
 // Called by a random walker task right before it finishes to suggest that another thread be allocated. 
 // This makes a poor man's thread pool. 
-void reassign_annotation_tasks(unordered_map<string, node *> &lookup_table) {
+void reassign_annotation_tasks(unordered_map<string, node *> &lookup_table, sp_fmat *category_matrix, float alpha) {
 	lock_guard<mutex> lock(task_reassign_lock); // mutex will be unlocked when this function exits
 
 	threads_working--; // we were called by a current thread about to stop	
@@ -307,7 +266,7 @@ void reassign_annotation_tasks(unordered_map<string, node *> &lookup_table) {
 		getline(*infile_global, next_category);
 
 		threads_working++;
-		thread t(random_walk, next_category, ref(lookup_table));
+		thread t(random_walk, next_category, ref(lookup_table), category_matrix, alpha);
 		t.detach();
 	}
 }
@@ -344,6 +303,23 @@ int main(int argc, char ** argv) {
 	}
 
 
+	// Construct the sparse matrix representing the category graph
+	// Note: the size of this matrix is n^2 in the number of categories. But most categories
+	// are not connected to each other so we store this as a sparse matrix. 
+	unsigned number_of_categories = lookup_table.size(); // number of nodes
+	sp_fmat *category_matrix = new sp_fmat(number_of_categories, number_of_categories);
+
+	// Graph structure: G(i,j) represents the probability of travelling from i to j in any given step. 
+	// Since this graph is unweighted, G(i,j) is simply:
+	//			1 / len(neighbors(i)) if i and j are neighbors
+	//			0 					  if i and j are not neighbors
+
+	// Note that this does not take into account the restart probability alpha, which is accounted for later. 
+
+	cerr << "Populating graph matrix...\n";
+	populate_graph_matrix(lookup_table, category_matrix, RWR_ALPHA);
+
+
 	// Data loaded, perform random walks
 	cerr << "Performing random walks for all categories...\n";
 	
@@ -352,7 +328,7 @@ int main(int argc, char ** argv) {
 	if (infile.good()) {
 		threads_working++;
 		infile_global = &infile;
-		reassign_annotation_tasks(lookup_table);
+		reassign_annotation_tasks(lookup_table, category_matrix, RWR_ALPHA);
 	}
 	
 	
